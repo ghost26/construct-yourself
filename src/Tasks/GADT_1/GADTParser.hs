0f7bdcd936fcff2ee1d9a0c7e9f6467bcbd5f487
Ruslan Abdulkhalikov <r.abdulkhalikov@corp.vk.com>
2017-11-18 18:36:03 +0300

    hm 3 without fix

--- src/Tasks/GADT_1/GADTParser.hs
+++ src/Tasks/GADT_1/GADTParser.hs
@@ -4,9 +4,9 @@
 
 module Tasks.GADT_1.GADTParser where
 
-import           Data.Text              (pack)
+import           Data.Text              (pack, unpack)
 import           Tasks.GADT_1.GADTExpr
-import           Text.Parsec.Char       (char, digit, space, string)
+import           Text.Parsec.Char       (char, digit, satisfy, space, string)
 import           Text.Parsec.Combinator (between, many1)
 import           Text.Parsec.Language   (haskellDef)
 import           Text.Parsec.Prim       (many, parseTest, try, (<|>))
@@ -14,10 +14,10 @@ import           Text.Parsec.Text       (Parser)
 import           Text.Parsec.Token
 
 iLitP :: Parser (Lit Int)
-iLitP = undefined
+iLitP = (ILit . read) <$> (\x -> bracketP x <|> x) (spacedP (many1 digit))
 
 bLitP :: Parser (Lit Bool)
-bLitP = undefined
+bLitP = (\x -> BLit (if x == 'T' then True else False)) <$> (\x -> bracketP x <|> x) (spacedP (satisfy (\x -> x == 'T' || x == 'F')))
 
 iiLitP :: Parser (Expr Int)
 iiLitP = Lit <$> iLitP
@@ -26,24 +26,26 @@ bbLitP :: Parser (Expr Bool)
 bbLitP = Lit <$> bLitP
 
 addP :: Parser (Expr Int)
-addP = undefined
+addP = (Add <$> (spacedP (bracketP parse) <* char '+') <*> spacedP parse) <|>
+       (Add <$> (iiLitP <* char '+') <*> spacedP parse)
 
 leqP :: Parser (Expr Bool)
 leqP = Leq <$> (parse <* char '<') <*> parse
 
 andP :: Parser (Expr Bool)
-andP = undefined
+andP = (And <$> (spacedP (bracketP parse) <* string "&&") <*> spacedP parse) <|>
+       (And <$> (bbLitP <* string "&&") <*> spacedP parse)
 
 spacedP :: Parser a -> Parser a
 spacedP p = (many space *> p) <* many space
 
 bracketP :: Parser a -> Parser a
-bracketP = undefined
+bracketP = try . between (char '(') (char ')')
 
 class MyParse a where
   parse :: Parser (Expr a)
 
 instance MyParse Int where
-  parse = undefined
+  parse = try (spacedP addP) <|> bracketP parse <|> iiLitP
 instance MyParse Bool where
-  parse = undefined
+  parse = try (spacedP leqP) <|> try (spacedP andP) <|> bracketP parse <|> bbLitP
